511b4475c195250891e1b76afaef4b4602170fd8
khoalun <buikhoa793@gmail.com>
2023-11-22 21:03:16 +0200

    Adding SSH

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://github.com/khoalun',
-  title: 'Kbs.',
+  title: 'fref.',
 }
 
 const about = {
